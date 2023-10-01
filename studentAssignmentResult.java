b303ca3bf67270fb14c64d1efdcb5c614fcc3309
prabigya09 <63800081+prabigya09@users.noreply.github.com>
2023-10-01 20:37:42 +1000

    Initial sharing of project

---
+++ studentAssignmentResult.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class studentAssignmentResult here.
+ *
+ * @author (Prabigya)
+ * @version (a version number or a date)
+ */
+public class studentAssignmentResult
+{
+
+}
