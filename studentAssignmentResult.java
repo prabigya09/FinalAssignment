ef9dcadeea5ecdb6669f9d079dae89bb3fc2145d
prabigya09 <63800081+prabigya09@users.noreply.github.com>
2023-10-01 20:48:59 +1000

    class student

--- studentAssignmentResult.java
+++ studentAssignmentResult.java
@@ -5,7 +5,19 @@
  * @author (Prabigya)
  * @version (a version number or a date)
  */
+import java.io.*;
+import java.util.*;
+
 public class studentAssignmentResult
 {
-
+  private String firstName;
+  private String lastName;
+  private int studentID;
+  private double[] marks;
+  private double totalMark;
+  
+  public studentAssignmentResult(String firstName, StringlastName, int studentID, private double[] marks, double totalMark) {
+      
+  }
+  
 }
