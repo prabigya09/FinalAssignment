475601c82820f30ba81ab7e97592831dd80306b8
prabigya09 <63800081+prabigya09@users.noreply.github.com>
2023-10-02 17:09:56 +1000

    F1

--- studentAssignmentResult.java
+++ studentAssignmentResult.java
@@ -5,19 +5,36 @@
  * @author (Prabigya)
  * @version (a version number or a date)
  */
-import java.io.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+//F1:
+3...........
 public class studentAssignmentResult
 {
+  private String unitName;
   private String firstName;
   private String lastName;
   private int studentID;
   private double[] marks;
   private double totalMark;
+  private List<Student> students = new ArrayList<>();
   
-  public studentAssignmentResult(String firstName, StringlastName, int studentID, private double[] marks, double totalMark) {
-      
-  }
+public void readDataFromFile( String fileName) {
+    try(BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+        String line;
+        boolean readUnitName = true;
+        while ((line = reader.readLine()) != null) {
+            if(line.startsWith("last name")) {
+                continue;
+            }
+        }
+    }
+}
+  
+  
   
 }
